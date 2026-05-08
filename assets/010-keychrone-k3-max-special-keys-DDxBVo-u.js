5e159b3eb799774155ecfe95190f9acc47681ef5
machingclee <machingclee@gmail.com>
2026-05-09 01:29:30 +0800

    update

---
+++ assets/010-keychrone-k3-max-special-keys-DDxBVo-u.js
@@ -0,0 +1,36 @@
+const n=`---
+title: "Keychrone K3 MAX SE Special Key Combinations"
+date: 2026-05-09
+id: personal0010
+tag: personal
+intro: Just a record of an image of special key combinations.
+indent: false
+toc: false
+img: tech
+---
+
+
+<style>
+  video {
+    border-radius: 4px;
+    max-width: 660px;
+  }
+  img {
+    max-width: 660px !important;
+  }
+
+  .label-container {
+    border: none;
+    stroke: none !important;
+    fill : transparent !important;
+  }
+  .edgeLabel {
+    padding: 10px;
+    background-color : rgb(248, 249, 250) !important;
+  }
+</style>
+
+
+The menu and the layout of the keyboard:
+
+![](/assets/img/2026-05-09-01-21-12.png)`;export{n as default};
